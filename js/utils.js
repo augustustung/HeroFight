56bac023639549232d99fd821bd2ff9c0b21ad1b
Augustus Flynn <huytung139@gmail.com>
2022-03-30 12:48:58 +0700

    demo game play

---
+++ js/utils.js
@@ -0,0 +1,114 @@
+function rectangularCollision({ rectangle1, rectangle2 }) {
+  return (
+    rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
+    rectangle2.position.x &&
+    rectangle1.attackBox.position.x <=
+    rectangle2.position.x + rectangle2.width &&
+    rectangle1.attackBox.position.y + rectangle1.attackBox.height >=
+    rectangle2.position.y &&
+    rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+  )
+}
+
+function determineWinner({ player, enemy, timerId }) {
+  clearTimeout(timerId);
+  document.querySelector('#displayText').style.display = 'flex'
+  if (player.health === enemy.health) {
+    document.querySelector('#displayText').innerHTML = 'Tie'
+  } else if (player.health > enemy.health) {
+    document.querySelector('#displayText').innerHTML = 'Player 1 Wins'
+  } else if (player.health < enemy.health) {
+    document.querySelector('#displayText').innerHTML = 'Player 2 Wins'
+  }
+
+  isStart = false;
+  buttonPlay.style.display = 'block';
+  buttonPlay.style.marginTop = '32%';
+  document.querySelector('#startGame p').textContent = 'Back to room'
+  buttonPlay.addEventListener('click', () => window.location.reload())
+}
+
+let timer = 60
+let timerId
+function decreaseTimer() {
+  if (timer > 0) {
+    timerId = setTimeout(decreaseTimer, 1000)
+    timer--
+    document.querySelector('#timer').innerHTML = timer
+  }
+
+  if (timer === 0) {
+    determineWinner({ player, enemy, timerId })
+  }
+}
+
+function startGame(e) {
+  e.preventDefault();
+  if (isStart) { return; }
+  decreaseTimer()
+
+  window.addEventListener('keydown', (event) => {
+    if (!player.dead) {
+      switch (event.key) {
+        case 'd':
+          keys.d.pressed = true
+          player.lastKey = 'd'
+          break
+        case 'a':
+          keys.a.pressed = true
+          player.lastKey = 'a'
+          break
+        case 'w':
+          player.velocity.y = -20
+          break
+        case ' ':
+          player.attack()
+          break
+      }
+    }
+
+    if (!enemy.dead) {
+      switch (event.key) {
+        case 'ArrowRight':
+          keys.ArrowRight.pressed = true
+          enemy.lastKey = 'ArrowRight'
+          break
+        case 'ArrowLeft':
+          keys.ArrowLeft.pressed = true
+          enemy.lastKey = 'ArrowLeft'
+          break
+        case 'ArrowUp':
+          enemy.velocity.y = -20
+          break
+        case 'ArrowDown':
+          enemy.attack()
+
+          break
+      }
+    }
+  })
+
+  window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+      case 'd':
+        keys.d.pressed = false
+        break
+      case 'a':
+        keys.a.pressed = false
+        break
+    }
+
+    // enemy keys
+    switch (event.key) {
+      case 'ArrowRight':
+        keys.ArrowRight.pressed = false
+        break
+      case 'ArrowLeft':
+        keys.ArrowLeft.pressed = false
+        break
+    }
+  })
+
+  buttonPlay.style.display = 'none';
+  isStart = true;
+}
